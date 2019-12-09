8a91bbbbc306f0ad32ca901b091a7d1816dff176
mariselagonzalez <37843508+mariselagonzalez@users.noreply.github.com>
2019-12-09 13:02:55 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var one= document.getElementById ("second")
+var two= document.getElementById ("third")
+var three= document.getElementById ("fourth")
+
+function imageone(){
+	one.classList.remove("hide");
+}
+function imagetwo(){
+	two.classList.remove("hide");
+}
+function imagethree(){
+	three.classList.remove("hide");
+}
